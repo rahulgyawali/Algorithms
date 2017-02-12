4a532ab4a4d83c4ed92c77966773f678890a356a
Rahul Gyawali <rahulgyawali@users.noreply.github.com>
2017-02-13 01:05:02 +0530

    Add files via upload

---
+++ Sorting/2D_QUICKSORT.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap (int* a,int* b)
+{
+	int temp;
+	
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int partition (int a[][2],int low,int high)
+{
+	int pivot;
+	int pindex;
+	int i;
+
+	pivot = a[high][0];
+	pindex = low;
+	
+	for (i = low; i < high; i++) {
+
+		if ((a[i][0] < pivot) || ((a[i][0] == pivot) && (a[i][1] < a[high][1]))) {
+	
+			swap(&a[i][0],&a[pindex][0]);
+			swap(&a[i][1],&a[pindex][1]);	
+			pindex++;	
+		}
+	}
+
+	swap(&a[high][0],&a[pindex][0]);
+	swap(&a[high][1],&a[pindex][1]);	
+
+	return pindex;
+}
+
+void quicksort (int a[][2],int low,int high)
+{
+	if (low < high) {
+
+		int pindex = partition(a,low,high);
+		quicksort(a,low,pindex-1);
+		quicksort(a,pindex+1,high);
+	}
+}
+
+int  main()
+{
+	int i;
+	int n;
+	int j;
+	
+	scanf("%d",&n);
+
+	int a[n][2];	
+
+	for (i = 0; i < n; i++) {
+
+		for (j = 0; j < 2; j++) {
+
+			scanf("%d",&a[i][j]);
+	
+		}
+	}
+
+	quicksort(a,0,n-1);
+
+	for (i = 0; i < n; i++) {
+
+		for (j = 0; j < 2; j++) {
+
+			printf("%d ",a[i][j]);
+	
+		}
+		printf("\n");
+	}
+	
+
+}
