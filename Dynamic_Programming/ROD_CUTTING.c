28ea0df2dd2c20a429cc47d642316f1682ad5cb7
Rahul Gyawali <rahulgyawali@users.noreply.github.com>
2017-02-23 23:36:57 +0530

    Update ROD_CUTTING.c

--- Dynamic_Programming/ROD_CUTTING.c
+++ Dynamic_Programming/ROD_CUTTING.c
@@ -16,7 +16,7 @@ int max (int a,int b)
 
 		return b;
 	}
-
+	
 	return a;	
 }
 
@@ -26,11 +26,12 @@ int main()
 	int i;
 	int n;
 	int j;
-	int k;
-
-	scanf("%d %d",&n,&k);
+	int x;
+	
+	scanf("%d",&n);
 
 	struct rod arr[n+1];
+	int b[n+1];
 	
 	for (i = 1; i <= n; i++) {
 
@@ -38,7 +39,7 @@ int main()
 		scanf("%d",&arr[i].value);
 	}
 
-	int b[n+1];
+	
 	
 	b[0] = 0;
 	b[1] = arr[1].value;
@@ -47,24 +48,20 @@ int main()
 
 		b[i] = 0;
 	}	
-	
-	int x;
 
 	for (i = 2; i <= n; i++) {
+		
+		x = arr[i].value;				//x stores maximum value for given lenght i
 
-		x = arr[i].value;
+		for (j = 1; j <= i; j++) {
 
-		for (j = 1;j <= i;j++) {
-
-			x = max(x,(arr[j].value + b[i-j]));
+			x = max(x,(arr[j].value + b[i-j]));	//every time we find maximum between max till now and particluar permutation for that lenght
 		}
 
 		b[i] = x;
-		
-
 	}
 
-	printf("\n%d is the maximum profit for length %d\n",b[n],n);
+	printf("%d is the maximum profit for length %d\n",b[n],n);
 
 	return 0;
 }
