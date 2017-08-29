416ee54dcf7eacbe679438f3058ce58f9b963bce
Rahul Gyawali <rahulgyawali@users.noreply.github.com>
2017-08-29 16:54:43 +0530

    Add files via upload

---
+++ Graph_Algorithms/bfs.py
@@ -0,0 +1,16 @@
+g =  []
+n = input()
+n = (int(n))
+
+for i in range(n):
+	    g.append([])
+	        
+		    for j in range(m):
+			    		            x=random.randrange(2)
+			            vis[i].append(0)
+					            if x==0:
+							                adj[i].append('C')
+									        else:
+											            adj[i].append('D')
+
+
