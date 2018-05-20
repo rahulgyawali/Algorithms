52a5dab3f55b32e326a30caa777eaa4d2a87a694
rahulgyawali <iit2015005@iiita.ac.in>
2018-05-20 13:22:18 +0530

    Need to make changes in this one

---
+++ Place/Bits/anag_comp.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int a;
+	cin>>a;
+	int b = ~a;
+	int arr[32];
+	int brr[32];
+
+	int i=0;
+	while(a) {
+		arr[i] = a%2;
+		a = a/2;
+		i++;
+	}
+	i = 0;
+	while(b) {
+		brr[i] = b%2;
+		b =b/2;
+		i++;
+	}
+	sort(arr,arr+32);
+	sort(brr,brr+32);
+	for(i = 0;i < 32; i++) {
+		if(arr[i] != brr[i]) {
+		
+			cout<<"NO"<<endl;
+			return 0;	
+		}
+	}
+	cout<<"YES"<<endl;
+	return 0;
+}
