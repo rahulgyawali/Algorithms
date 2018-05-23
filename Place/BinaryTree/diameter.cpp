1a11dcb272eb3f6d225727e452ee9525061d5c39
rahulgyawali <iit2015005@iiita.ac.in>
2018-05-23 13:17:20 +0530

    Binary Tree

---
+++ Place/BinaryTree/diameter.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct node {
+	int data;
+	struct node* left;
+	struct node* right;
+};
+
+int height(struct node* root)
+{
+	if(root == NULL)
+		return -1;
+	return (1 + ((height(root->left) > height(root->right) ) ? height(root->left) : height(root->right));) 
+}
+
+struct node* getnode(int x)
+{
+	struct node* tmp = (struct node*)malloc(sizeof(struct node));
+	tmp->left = NULL;
+	tmp->right = NULL;
+	tmp->data = x;
+
+	return tmp;
+}
+
+
+int main()
+{
+	struct node* root = getnode(1);
+	root->left = getnode(2);
+	root->right = getnode(3);
+	root->left->left = getnode(4);
+
+
+	return 0;
+}
