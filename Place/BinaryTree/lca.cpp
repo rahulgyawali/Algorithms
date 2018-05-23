d5feaa33060a73a150d77420e3f0e0265f3bd0a5
rahulgyawali <iit2015005@iiita.ac.in>
2018-05-23 19:39:37 +0530

    Added LCA

--- Place/BinaryTree/lca.cpp
+++ Place/BinaryTree/lca.cpp
@@ -7,13 +7,6 @@ struct node {
 	struct node* right;
 };
 
-int height(struct node* root)
-{
-	if(root == NULL)
-		return -1;
-	return (1 + ((height(root->left) > height(root->right) ) ? height(root->left) : height(root->right))); 
-}
-
 struct node* getnode(int x)
 {
 	struct node* tmp = (struct node*)malloc(sizeof(struct node));
@@ -24,7 +17,22 @@ struct node* getnode(int x)
 	return tmp;
 }
 
+struct node* lca(struct node* root,int n1,int n2)
+{
+	if(root == NULL) return NULL;
 
+	if(root->data == n1 || root->data == n2) {
+
+		return root;
+	}
+
+	struct node* l = lca(root->left,n1,n2);
+	struct node* r = lca(root->right,n1,n2);
+
+	if(l && r) return root;
+
+	return (l!=NULL)?l:r;
+}
 
 int main()
 {
@@ -32,9 +40,10 @@ int main()
 	root->left = getnode(2);
 	root->right = getnode(3);
 	root->left->left = getnode(4);
+	root->left->left->left = getnode(6);
 	root->left->right = getnode(5);
 	
-	cout<<"The Height is "<<height(root);
+	cout<<"The LCA is "<<(lca(root,5,3))->data<<endl;
 	
 
 	return 0;
