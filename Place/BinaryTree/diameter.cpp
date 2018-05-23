410dbc2695ae0557495987ac1954c30cdbf65e86
rahulgyawali <iit2015005@iiita.ac.in>
2018-05-23 13:24:11 +0530

    Height and level order

--- Place/BinaryTree/diameter.cpp
+++ Place/BinaryTree/diameter.cpp
@@ -11,7 +11,7 @@ int height(struct node* root)
 {
 	if(root == NULL)
 		return -1;
-	return (1 + ((height(root->left) > height(root->right) ) ? height(root->left) : height(root->right));) 
+	return (1 + ((height(root->left) > height(root->right) ) ? height(root->left) : height(root->right))); 
 }
 
 struct node* getnode(int x)
@@ -24,6 +24,34 @@ struct node* getnode(int x)
 	return tmp;
 }
 
+void levelorder(struct node* root)
+{
+	queue<struct node*> q;
+	int n;
+
+	q.push(root);
+	
+	while(!q.empty()) {
+		
+		n = q.size();
+
+		while(n--) {
+
+			struct node* temp = q.front();
+			q.pop();
+			cout<<temp->data<<" ";
+
+			if(temp->left) {
+				q.push(temp->left);
+			}
+
+			if(temp->right) {
+				q.push(temp->right);
+			}
+		}
+		cout<<endl;
+	}
+}
 
 int main()
 {
@@ -31,7 +59,12 @@ int main()
 	root->left = getnode(2);
 	root->right = getnode(3);
 	root->left->left = getnode(4);
+	root->left->right = getnode(5);
+	
+	cout<<"The Height is "<<height(root);
+	cout<<"The Level Wise"<<endl;
 
+	levelorder(root);
 
 	return 0;
 }
