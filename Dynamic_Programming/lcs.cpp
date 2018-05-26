d2b208ace9aeaa835efc15ab36a51a6456a89e4d
rahulgyawali <iit2015005@iiita.ac.in>
2018-05-27 01:13:18 +0530

    Printing LCS

--- Dynamic_Programming/lcs.cpp
+++ Dynamic_Programming/lcs.cpp
@@ -12,8 +12,9 @@ int lcs(string X,string Y)
     int m = X.size();
     int n = Y.size();
     int i,j;
-    int LCS[m+1][n+1] = {0} ;
+    int LCS[m+1][n+1];
 
+    memset(LCS,0,sizeof(int)*(m+1)*(n+1));
     for(i = 1; i <= m; i++) {
 
        for(j = 1; j <= n; j++) {
@@ -29,6 +30,32 @@ int lcs(string X,string Y)
 	}
     }
 
+   i = m;
+   j = n;
+   stack<char> ss;
+   while(i > 0 && j > 0) {
+	
+	   if(LCS[i][j] > LCS[i][j-1] && LCS[i][j] > LCS[i-1][j]) {
+		ss.push(X[i-1]);
+		i--;
+		j--;
+	   }else{
+
+	   	if(LCS[i-1][j] > LCS[i][j-1]) {
+			i--;
+		}else {
+			j--;
+		}
+	   }
+   }
+
+    while(!ss.empty()) {
+	cout<<ss.top();
+	ss.pop();
+    }
+    
+    cout<<endl;
+
     return LCS[m][n];
 }
 
@@ -36,7 +63,8 @@ int main()
 {
     string x = "abcdaf";
     string y =	"acbc";
-    cout<<lcs(x,y);
+    cout<<lcs(x,y)<<endl;
+    
     return 0;
 }
 
