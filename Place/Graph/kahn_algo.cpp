bd0e524d08292078a3ec296bbe4f9b6d84d870f1
rahulgyawali <iit2015005@iiita.ac.in>
2018-05-31 00:19:22 +0530

    Works fine ;)

--- Place/Graph/kahn_algo.cpp
+++ Place/Graph/kahn_algo.cpp
@@ -30,9 +30,44 @@ int main()
 		
 		if(indegree[i] == 0) {
 		
-
+			q.push(i);
+			
 		}
 	}
+
+	int count = 0;
+
+	while(!q.empty()) {
+
+		int x = q.front();
+		q.pop();
+		topo.push_back(x);
+
+		for(i = 0; i < g[x].size(); i++) {
+
+			--indegree[g[x][i]];
+			if(indegree[g[x][i]] == 0) {
+
+				q.push(g[x][i]);
+			}
+		}
+
+		count++;
+	}
+	if(n != count) {
+		cout<<"There's a Cycle :( "<<endl;
+		return 0;
+	}else{
+
+		cout<<"Topological Sort "<<endl;
+
+		for(i = 0; i < n; i++) {
+
+			cout<<topo[i]<<endl;
+		}
+	}
+
+	
 
 	return 0;
 
